20b4eca047cb6af4f66e60d372becaad945593ae
Burton-David <dgburton@pm.me>
2020-04-25 14:55:12 -0400

    removed old files

--- venv/lib/python3.7/site-packages/translate/providers/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
-from .mymemory_translated import MyMemoryProvider  # noqa
-from .microsoft import MicrosoftProvider  # noqa
-
-__all__ = ['MyMemoryProvider', 'MicrosoftProvider']
